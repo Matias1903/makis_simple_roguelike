15dd077467810d90fc50e525cbe1614b244f4b52
matifinochio <matifinochio@gmail.com>
2022-12-11 23:38:20 -0300

    First Commit

---
+++ src/player.rs
@@ -0,0 +1,37 @@
+use rltk::{VirtualKeyCode,Rltk};
+use specs::prelude::*;
+use crate::Viewshed;
+
+use super::{Position, Player, TileType, Map, State};
+use std::cmp::{max, min};
+
+pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World){
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    let mut viewsheds = ecs.write_storage::<Viewshed>();
+    let map = ecs.fetch::<Map>();
+
+    for (_player, pos, viewshed) in (&mut players, &mut positions, &mut viewsheds).join() {
+        let destination_idx = map.xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map.tiles[destination_idx] != TileType::Wall{
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+
+            viewshed.dirty = true;
+        }
+    }
+}
+
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    //Player Movement
+    match ctx.key{
+        None => {}
+        Some(key) => match key{
+            VirtualKeyCode::A => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::D => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::W => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::S => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
